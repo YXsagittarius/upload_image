b40da51f9a8f4427396ece5c6946bfb1722e05ee
YXsagittarius <yangxiao@getqujing.com>
2015-04-28 20:45:04 +0800

    image upload

---
+++ app/models/image.rb
@@ -0,0 +1,6 @@
+class Image < ActiveRecord::Base
+  has_attached_file :avatar, :styles => {
+                      :thumb => "75x75>",
+                      :small => "150x150>"
+                    }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UploadImage::Application.config.secret_key_base = '2e55711a375fdd0d65f96ee768badcd3e60c607c4bcad8a1e72329107b305b7713c19835b5ca858713eececaa4d133cff251ddefb70de9bdbce29b55e9937899'
